d84f5ec8f72e546b112628e920d23c83d5844943
fdolzanes1 <fdolzanes1@gmail.com>
2021-01-27 20:08:43 -0400

    Added index person

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-  resources :people, only: [:show]
+  resources :people, only: [:show, :index]
 end
