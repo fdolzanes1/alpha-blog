0160ac8a52db4340e80e3b00a5c881867e42b3dc
fdolzanes1 <fdolzanes1@gmail.com>
2021-01-30 14:11:39 -0400

    Added animal controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   resources :people, only: [:show, :index, :new, :create]
+  resources :animals, only: [:index, :show]
 end
