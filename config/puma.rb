36d2fc3aa40088e91866c0e40ccd4839515d3fc6
fdolzanes1 <fdolzanes1@gmail.com>
2021-02-02 19:23:52 -0400

    Updated

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -22,7 +22,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
+# Workers do not work on JRuby or Windows (both of which do not support 
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
