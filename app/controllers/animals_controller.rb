e79c0515e662f85e582fc187de1af93737c5db50
fdolzanes1 <fdolzanes1@gmail.com>
2021-02-04 19:47:29 -0400

    Added Update and delete animals

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,5 +1,7 @@
 class AnimalsController < ApplicationController
 
+  before_action :set_animal, only: [:show, :edit, :update, :destroy]
+
   def index
     @animals = Animal.all
   end
@@ -21,7 +23,25 @@ class AnimalsController < ApplicationController
     end  
   end
 
+  def update
+    if @animal.update(animal_params)
+      flash[:notice] = 'animal was updated'
+      redirect_to @animal
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @animal.destroy
+    redirect_to animals_path
+  end
+
   private
+
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
 
   def animal_params
     params.require(:animal).permit(:name, :category, :color, :age)
