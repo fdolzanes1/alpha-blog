43120c6fa69260afa46ecaf14f7a39a4b54a78f0
fdolzanes1 <fdolzanes1@gmail.com>
2021-03-02 17:04:12 -0400

    added auth methods to controller as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	helper_method :current_person, :logged_in?
+
+	def current_person
+		@current_person ||= Person.find(session[:person_id]) if session[:person_id]
+	end
+
+	def logged_in?
+	!!current_person
+	end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-    @article.person = Person.first
+    @article.person = current_person
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
